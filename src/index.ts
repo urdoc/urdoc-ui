8b85c9e7ec8da143c6f2c542fde9b2fb3e052a52
aki-webii <c.mikamichel@gmail.com>
2019-10-09 14:54:37 +0900

    fix: Some point for npm publish ready.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './components/Input/Input';
-export * from './components/TextArea/TextArea';
-export * from './components/Select/Select';
-export * from './components/Button';
+export { Input } from './components/Input';
+export { TextArea } from './components/TextArea';
+export { Select } from './components/Select';
+export { Button } from './components/Button';

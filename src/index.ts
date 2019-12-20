d1a1dd8854b8e359f9e591bafe66303b68c9452e
aki-webii <c.mikamichel@gmail.com>
2019-12-20 16:26:21 +0900

    feat: Add Dimmer component.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Input } from './components/Input';
 export { TextArea } from './components/TextArea';
 export { Select } from './components/Select';
 export { Button } from './components/Button';
+export { Dimmer } from './components/Dimmer';

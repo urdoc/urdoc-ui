d7b271d1a0518cd57a155b8df461a8f7d013b843
yuto sakurai <ys.p1xb1t@gmail.com>
2019-12-22 02:53:44 +0900

    added H1

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Input } from './components/Input';
 export { TextArea } from './components/TextArea';
 export { Select } from './components/Select';
 export { Button } from './components/Button';
+export { H1 } from './components/Headings/H1';
